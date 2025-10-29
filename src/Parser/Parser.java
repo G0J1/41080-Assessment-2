8000f5ae27542415fee96c15c9b2c8472177e5f5
Daniel Mercuri <Daniel.L.Mercuri@student.uts.edu.au>
2025-10-29 21:17:49 +1100

    4thcommit
    test

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Parser {
 
     public static List<Token> tokenize(String input) {
-        System.out.println("Runs!");
+        System.out.println("Runs! ");
 
         return Collections.<Token>emptyList();
     }
